6c80e0db78f66bcee76bae97f7c9c714b6fbdaf3
DanyGlez00 <dgonzalez@griddynamics.com>
2023-06-16 14:18:52 -0600

    :sparkles: [ADD]: Navbar component added

--- twitter-clone/src/components/Navbar/Navbar.tsx
+++ twitter-clone/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Navbar.scss';
 import { FaTwitter } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='navbar__left'>
-        <FaTwitter />
+        <FaTwitter className='navbar__left-logo'/>
         <p>Another Twitter Clone</p>
       </div>
       <div className='navbar__right'>
         <p>John Smith</p>
-        <p>JS</p>
+        <p className='navbar__right-initials'>JS</p>
       </div>
     </nav>
   );
