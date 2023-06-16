ffebc48ebb6e81dc12d289f52caf9ab3d7587774
DanyGlez00 <dgonzalez@griddynamics.com>
2023-06-16 15:28:15 -0600

    :sparkles: [ADD]: Button component added

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import TweetCard from './components/TweetCard/TweetCard';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <TweetCard/>
+      <Button/>
     </div>
   );
 }
---
+++ twitter-clone/src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button.scss";
+
+const Button = () => {
+  return (
+    <button className="button">
+      <p className="button__text">Tweet</p>
+    </button>
+  );
+};
+
+export default Button;
