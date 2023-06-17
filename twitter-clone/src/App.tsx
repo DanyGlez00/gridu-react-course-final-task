e7fa92dc9438f761e9c0c37ef997331d6a36ef89
DanyGlez00 <dgonzalez@griddynamics.com>
2023-06-16 23:53:53 -0600

    :sparkles: [FEAT]: React Router added and working

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
-import TweetCard from './components/TweetCard/TweetCard';
-import Button from './components/Button/Button';
 import SignUp from './pages/SignUp/SignUp';
-import Form from './components/Form/Form';
 import Login from './pages/Login/Login';
-import Input from './components/Input/Input';
-// import './_variables.scss';
 
 function App() {
   return (
-    <div>
-      {/* <Navbar/> */}
-      {/* <TweetCard/> */}
-      {/* <Button title='Tweet' width='50%' type='submit'/> */}
-      {/* <SignUp/> */}
-      <Login/>
-      {/* <Form title="Sign up" paragraphText="Don't have an account?" inputs={inputs} /> */}
-    </div>
+    <Routes>
+      <Route path="/" element={<Navbar />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
