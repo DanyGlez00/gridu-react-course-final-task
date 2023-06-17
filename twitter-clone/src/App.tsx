67f595b178e464159e1645f02cd75a3356ecf977
DanyGlez00 <dgonzalez@griddynamics.com>
2023-06-16 18:29:30 -0600

    :sparkles: [ADD]: Log in component added

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import TweetCard from './components/TweetCard/TweetCard';
 import Button from './components/Button/Button';
 import SignUpForm from './components/SignUpForm/SignUpForm';
+import Login from './pages/Login/Login';
+// import './_variables.scss';
 
 function App() {
   return (
     <div>
-      {/* <Navbar/>
-      <TweetCard/>
-      <Button/> */}
-      <SignUpForm/>
+      {/* <Navbar/> */}
+      {/* <TweetCard/> */}
+      {/* <Button/> */}
+      {/* <SignUpForm/> */}
+      <Login/>
     </div>
   );
 }
